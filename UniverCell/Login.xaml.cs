df02986d66ef3e1fe39d130980010afe8411a381
LuisHCK <luisjcenteno@live.com>
2016-12-08 01:25:06 -0600

    Si el login es correcto se guarda los datos del usuario en la clase sesion para ser ultilizada en las otras ventanas

--- UniverCell/Login.xaml.cs
+++ UniverCell/Login.xaml.cs
@@ -20,42 +20,75 @@ namespace UniverCell
     /// </summary>
     public partial class Login : Window
     {
+        Conexion CON = new Conexion();
+
         public Login()
         {
             InitializeComponent();
         }
 
-        public void button_Click(object sender, RoutedEventArgs e)
+        /*La funcion obtiene los datos de un usuario basado en el nombre de usuario y lo almacena en 
+         * la clase estatica sesión
+         */
+        public void DatosUsuario(string usuario)
         {
             try
             {
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM cellmax.usuarios where nombre_usuario = '"+usuario+"';", CON.conect);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Sesion.id_usuario = reader.GetString("id");
+                    Sesion.nomb_usuario = reader.GetString("nombre_usuario");
+                    Sesion.nomb_completo = reader.GetString("nombre_completo");
+                    Sesion.puest = reader.GetString("puesto");
+                    Sesion.dir = reader.GetString("direccion");
+                    Sesion.lvl = reader.GetString("lvl");
+                    Sesion.ced = reader.GetString("cedula");
+                    Sesion.fech_ingr = reader.GetString("fecha_ingreso");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error" + ex);
+            }
+        }
+
+        public void button_Click(object sender, RoutedEventArgs e)
+        {
+            string usr = txt_user.Text;
+            MessageBox.Show(usr);
+            try
+            {
                 //Conectar con la base de datos para consultar el inventaio
-                Conexion CON = new Conexion();
                 CON.conect.Open();
                 MySqlCommand cmd = new MySqlCommand();
 
                 cmd.Connection = CON.conect;
 
-                //cmd.CommandText = "select cellmax.login('" + txt_user.Text + "', '" + txt_pass.Password + "')";
-                //loguear autimaticamente (temporal)
-                cmd.CommandText = "select cellmax.login('luishck', '123')";
-
+                cmd.CommandText = "select cellmax.login('" + txt_user.Text + "', '" + txt_pass.Password + "')";
+                /*loguear autimaticamente (temporal)
+                *cmd.CommandText = "select cellmax.login('luishck', '123')";
+                */
                 int login = int.Parse(cmd.ExecuteScalar().ToString());
-                CON.conect.Close();
-
+                
                 //Si el login es correcto mandar a la ventana principal
                 if (login == 1)
                 {
-                    this.Hide();
-                    MainWindow Form = new MainWindow();
-                    Form.lbl_usuario.Content = this.txt_user.Text;
-                    Form.Show();
+                    DatosUsuario(usr);
+
+                    MainWindow VentPrincipal = new MainWindow();
+                    VentPrincipal.Show();
+                    this.Close();
                 }
                 //Caso contrario mandar un mensaje de error
                 else
                 {
                     MessageBox.Show("Usuario Incorrecto");
                 }
+
+                CON.conect.Close();
             }
             //Si ocurre un error con la conexion a la BD mandar una excepcion
             catch (MySqlException ex)
@@ -63,7 +96,6 @@ namespace UniverCell
                 MessageBox.Show("Ocurió un error" + ex);
             }
         }
-
     }
 }
 
